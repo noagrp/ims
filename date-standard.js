c849df4a40175cd05f4d541126d2c5efe2dfc98e
noagrp <lihwlee11@gmail.com>
2026-08-27 15:24:59 +0800

    Load commercial field enhancements

--- date-standard.js
+++ date-standard.js
@@ -70,6 +70,7 @@
   import('./scalable-logs.js').catch(err=>console.warn('IMS scalable logs unavailable:',err));
   import('./item-detail-history.js').catch(err=>console.warn('IMS targeted item history unavailable:',err));
   import('./layout-refinements.js').catch(err=>console.warn('IMS layout refinements unavailable:',err));
+  import('./commercial-fields.js').catch(err=>console.warn('IMS commercial fields unavailable:',err));
   import('./lifecycle-actions.js').catch(err=>console.warn('IMS lifecycle actions unavailable:',err));
   import('./item-open-tracker.js').catch(err=>console.warn('IMS item-open tracking unavailable:',err));
 })();
