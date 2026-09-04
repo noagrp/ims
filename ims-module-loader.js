43ec74a498f98a209c63d86f40034848ce8897e2
noagrp <lihwlee11@gmail.com>
2026-09-05 00:53:50 +0800

    Bump frontend build for client PO cycle dates

--- ims-module-loader.js
+++ ims-module-loader.js
@@ -1,6 +1,6 @@
 import { can, currentRole } from './ims-permissions.js';
 
-const IMS_BUILD='20260904-14';
+const IMS_BUILD='20260905-01';
 const versioned=src=>`${src}${src.includes('?')?'&':'?'}v=${IMS_BUILD}`;
 
 const MODULES=Object.freeze([
