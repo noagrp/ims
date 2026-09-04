c2e57ec5756c18c44b641f91a612db676ac7338c
noagrp <lihwlee11@gmail.com>
2026-09-04 15:41:07 +0800

    Bump IMS build for service quantity normalization

--- ims-module-loader.js
+++ ims-module-loader.js
@@ -1,6 +1,6 @@
 import { can, currentRole } from './ims-permissions.js';
 
-const IMS_BUILD='20260904-7';
+const IMS_BUILD='20260904-8';
 const versioned=src=>`${src}${src.includes('?')?'&':'?'}v=${IMS_BUILD}`;
 
 const MODULES=Object.freeze([
