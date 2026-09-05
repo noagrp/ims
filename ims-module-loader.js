8371e548ab6d400931de8bffd0c274f499a1f793
noagrp <lihwlee11@gmail.com>
2026-09-06 02:29:01 +0800

    Bump build for simplified Audit Firestore filter

--- ims-module-loader.js
+++ ims-module-loader.js
@@ -1,6 +1,6 @@
 import { can, currentRole } from './ims-permissions.js';
 
-const IMS_BUILD='20260906-13';
+const IMS_BUILD='20260906-14';
 const versioned=src=>`${src}${src.includes('?')?'&':'?'}v=${IMS_BUILD}`;
 
 const MODULES=Object.freeze([
