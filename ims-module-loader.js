9215658de1a48455f3c66654671f60285b2c21aa
noagrp <lihwlee11@gmail.com>
2026-09-07 13:03:16 +0800

    Bump IMS build for R2R workflow update

--- ims-module-loader.js
+++ ims-module-loader.js
@@ -1,6 +1,6 @@
 import { can, currentRole } from './ims-permissions.js';
 
-const IMS_BUILD='20260907-06';
+const IMS_BUILD='20260907-07';
 const versioned=src=>`${src}${src.includes('?')?'&':'?'}v=${IMS_BUILD}`;
 
 const MODULES=Object.freeze([
