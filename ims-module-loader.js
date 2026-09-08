d2a95e8795aeac9c73ddb27adf656cff6eca4fea
noagrp <lihwlee11@gmail.com>
2026-09-08 16:55:12 +0800

    Bump IMS build for workspace and item detail improvements

--- ims-module-loader.js
+++ ims-module-loader.js
@@ -1,6 +1,6 @@
 import { can, currentRole } from './ims-permissions.js';
 
-const IMS_BUILD='20260908-16';
+const IMS_BUILD='20260908-17';
 const versioned=src=>`${src}${src.includes('?')?'&':'?'}v=${IMS_BUILD}`;
 
 const MODULES=Object.freeze([
