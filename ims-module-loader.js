8532ecaba1e676b80eb53ab9c855670158b6197c
noagrp <lihwlee11@gmail.com>
2026-09-08 15:13:07 +0800

    Bump IMS build for inactive stock tab

--- ims-module-loader.js
+++ ims-module-loader.js
@@ -1,6 +1,6 @@
 import { can, currentRole } from './ims-permissions.js';
 
-const IMS_BUILD='20260907-15';
+const IMS_BUILD='20260908-16';
 const versioned=src=>`${src}${src.includes('?')?'&':'?'}v=${IMS_BUILD}`;
 
 const MODULES=Object.freeze([
