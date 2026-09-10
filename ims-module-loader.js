46989c37c65d9ca9bfccc1eb949e9dcff66502b5
noagrp <lihwlee11@gmail.com>
2026-09-10 12:23:21 +0800

    Bump IMS build for authoritative CSV toolbar

--- ims-module-loader.js
+++ ims-module-loader.js
@@ -1,6 +1,6 @@
-import { can, currentRole } from './ims-permissions.js?v=20260910-21';
+import { can, currentRole } from './ims-permissions.js?v=20260910-22';
 
-const IMS_BUILD='20260910-21';
+const IMS_BUILD='20260910-22';
 const versioned=src=>`${src}${src.includes('?')?'&':'?'}v=${IMS_BUILD}`;
 
 const MODULES=Object.freeze([
