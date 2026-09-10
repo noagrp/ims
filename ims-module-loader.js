33f54c86e748dec150746c5cd2efefe0113ecc51
noagrp <lihwlee11@gmail.com>
2026-09-10 11:29:33 +0800

    Bump IMS module build for CSV import fix

--- ims-module-loader.js
+++ ims-module-loader.js
@@ -1,6 +1,6 @@
-import { can, currentRole } from './ims-permissions.js?v=20260910-14';
+import { can, currentRole } from './ims-permissions.js?v=20260910-15';
 
-const IMS_BUILD='20260910-14';
+const IMS_BUILD='20260910-15';
 const versioned=src=>`${src}${src.includes('?')?'&':'?'}v=${IMS_BUILD}`;
 
 const MODULES=Object.freeze([
