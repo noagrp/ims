5f533d3817fda3d7b484a36d8c3bc89620d2383d
noagrp <lihwlee11@gmail.com>
2026-09-10 12:27:50 +0800

    Bump build for R2R CSV attachment fix

--- ims-module-loader.js
+++ ims-module-loader.js
@@ -1,6 +1,6 @@
-import { can, currentRole } from './ims-permissions.js?v=20260910-22';
+import { can, currentRole } from './ims-permissions.js?v=20260910-23';
 
-const IMS_BUILD='20260910-22';
+const IMS_BUILD='20260910-23';
 const versioned=src=>`${src}${src.includes('?')?'&':'?'}v=${IMS_BUILD}`;
 
 const MODULES=Object.freeze([
