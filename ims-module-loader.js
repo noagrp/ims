7c7d2c4861e9e51eceec389fcbd854189711afa8
noagrp <lihwlee11@gmail.com>
2026-09-10 10:47:14 +0800

    Bump IMS build for CSV field order alignment

--- ims-module-loader.js
+++ ims-module-loader.js
@@ -1,6 +1,6 @@
-import { can, currentRole } from './ims-permissions.js?v=20260910-12';
+import { can, currentRole } from './ims-permissions.js?v=20260910-13';
 
-const IMS_BUILD='20260910-12';
+const IMS_BUILD='20260910-13';
 const versioned=src=>`${src}${src.includes('?')?'&':'?'}v=${IMS_BUILD}`;
 
 const MODULES=Object.freeze([
