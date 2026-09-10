b3371dd1964de548852cb2ea2c0f9b2591bb3c23
noagrp <lihwlee11@gmail.com>
2026-09-10 16:53:53 +0800

    Bump IMS build for supplier PO linkage fix

--- ims-module-loader.js
+++ ims-module-loader.js
@@ -1,6 +1,6 @@
-import { can, currentRole } from './ims-permissions.js?v=20260910-27';
+import { can, currentRole } from './ims-permissions.js?v=20260910-28';
 
-const IMS_BUILD='20260910-27';
+const IMS_BUILD='20260910-28';
 const versioned=src=>`${src}${src.includes('?')?'&':'?'}v=${IMS_BUILD}`;
 
 const MODULES=Object.freeze([
