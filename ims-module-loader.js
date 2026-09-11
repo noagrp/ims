3ea38f0962509d554ca10e60fa2a2fbd99998ff3
noagrp <lihwlee11@gmail.com>
2026-09-11 15:35:38 +0800

    Bump IMS build for service cycle stability fix

--- ims-module-loader.js
+++ ims-module-loader.js
@@ -1,6 +1,6 @@
-import { can, currentRole } from './ims-permissions.js?v=20260911-34';
+import { can, currentRole } from './ims-permissions.js?v=20260911-35';
 
-const IMS_BUILD='20260911-34';
+const IMS_BUILD='20260911-35';
 const versioned=src=>`${src}${src.includes('?')?'&':'?'}v=${IMS_BUILD}`;
 
 const MODULES=Object.freeze([
