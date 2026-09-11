91b52745d4d90f27541041a96b93716b59302f9d
noagrp <lihwlee11@gmail.com>
2026-09-11 23:19:54 +0800

    Bump IMS build for aligned import export

--- ims-module-loader.js
+++ ims-module-loader.js
@@ -1,6 +1,6 @@
-import { can, currentRole } from './ims-permissions.js?v=20260911-37';
+import { can, currentRole } from './ims-permissions.js?v=20260911-38';
 
-const IMS_BUILD='20260911-37';
+const IMS_BUILD='20260911-38';
 const versioned=src=>`${src}${src.includes('?')?'&':'?'}v=${IMS_BUILD}`;
 
 const MODULES=Object.freeze([
