92299a35fddc34fa4ab3b2752393cee53d96fa1a
noagrp <lihwlee11@gmail.com>
2026-09-11 16:53:30 +0800

    Bump IMS build for registration import location fix

--- ims-module-loader.js
+++ ims-module-loader.js
@@ -1,6 +1,6 @@
-import { can, currentRole } from './ims-permissions.js?v=20260911-36';
+import { can, currentRole } from './ims-permissions.js?v=20260911-37';
 
-const IMS_BUILD='20260911-36';
+const IMS_BUILD='20260911-37';
 const versioned=src=>`${src}${src.includes('?')?'&':'?'}v=${IMS_BUILD}`;
 
 const MODULES=Object.freeze([
